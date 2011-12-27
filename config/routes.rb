dc4c2894f3a944cf1404b215a97d56496a37fbeb
jclosure <jclosure@gmail.com>
2011-12-27 01:09:52 -0600

    added comments to posts.  hooked into posts ui as partials for create and list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Blog::Application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
+  
+
 
   get "home/index"
+    
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111216062917) do
+ActiveRecord::Schema.define(:version => 20111227054705) do
 
-  create_table "posts", :force => true do |t|
-    t.string   "name"
-    t.string   "title"
-    t.text     "content"
+  create_table "comments", :force => true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
+
+  create_table "posts", :force => true do |t|
+    t.string    "name"
+    t.string    "title"
+    t.text      "content"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
 end
